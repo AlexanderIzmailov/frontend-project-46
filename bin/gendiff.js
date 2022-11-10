6d464eafb917d98590369693b8c496921c988d57
AlexanderIzmailov <a.a.izmailov@gmail.com>
2022-11-10 20:29:08 +0300

    Added showing diff in console

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 
-import gendiff from '../src/gendiff.js';
+import makeConsoleDiff from '../src/gendiff.js';
+import { Command } from "commander";
 
-gendiff();
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => makeConsoleDiff(filepath1, filepath2));
+
+program.parse();
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,16 +1,134 @@
-import { Command } from "commander";
+import * as fs from 'fs';
 
-const program = new Command();
+// const file1 = {
+//   "host": "hexlet.io",
+//   "timeout": 50,
+//   "proxy": "123.234.53.22",
+//   "follow": false
+// };
 
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+// const file2 = {
+//   "timeout": 20,
+//   "verbose": true,
+//   "host": "hexlet.io"
+// };
 
-program.parse();
-
-const gendiff = () => {
-    console.log("Enter");
+const checkPresenceSecondKey = (object, key, firstValue) => {
+  if (key in object) {
+    const secondValue = object[key]
+    return firstValue === secondValue ? 'similar' : 'different'
+  }
+  return 'onlyFirst';
 }
 
-export default gendiff;
+
+const makeDiffObject = (file1, file2, tree = {}, isSecondIteration = false) => {
+  
+  let diff = {}
+  if ('children' in tree) {
+    diff = {...tree};
+  } else {
+    diff.children = []
+  }
+
+  const alreadyExist = diff.children.map((item) => item.key)
+  const presence = isSecondIteration ? 'onlySecond' : 'onlyFirst';
+
+  for (const [key, firstValue] of Object.entries(file1)) {
+  
+    if (alreadyExist.indexOf(key) >= 0) {
+      continue;
+    }
+
+    const checkSecondKey = checkPresenceSecondKey(file2, key, firstValue);
+    let newDiffItem = {}
+
+    switch (checkSecondKey) {
+      case 'onlyFirst':
+        newDiffItem = {key, value: firstValue, presence, children:[]};
+        break;
+      case 'different':
+        const secondValue = file2[key]
+        newDiffItem = {key, firstValue, secondValue, presence:'bothDifferent', children:[]}
+        break;
+      case 'similar':
+        newDiffItem = {key, value: firstValue, presence:'bothSame', children:[]}
+    }
+
+    // let newDiffItem = {}
+    // if (key in file2) {
+    //   const secondValue = file2[key]
+
+    //   if (firstValue === secondValue) {
+    //     newDiffItem = {key, value: firstValue, presence:'bothSame', children:[]}
+    //   } else {
+    //     newDiffItem = {key, firstValue, secondValue, presence:'bothDifferent', children:[]}
+    //   }
+
+    // } else {
+    //   newDiffItem = {key, value: firstValue, presence, children:[]}
+    // }
+
+    diff.children.push(newDiffItem)
+  }
+
+  const result = isSecondIteration ? diff : makeDiffObject(file2, file1, diff, true);
+
+  return result
+}
+
+const takeData = (file) => {
+  const splittedFileName = file.split('.')
+  const extension = splittedFileName[splittedFileName.length - 1]
+
+  let data;
+
+  switch (extension) {
+    case 'json':
+      data = JSON.parse(fs.readFileSync(file))
+      break
+    default:
+      data = JSON.parse(fs.readFileSync(file))
+  }
+
+  return data
+}
+
+
+const makeConsoleDiff = (file1, file2) => {
+
+  const dataFile1 = takeData(file1)
+  const dataFile2 = takeData(file2)
+
+  const diffObject = makeDiffObject(dataFile1, dataFile2)
+
+  const result = diffObject.children
+  .sort((a, b) => {
+    const keyA = a.key.toUpperCase();
+    const keyB = b.key.toUpperCase();
+    if (keyA < keyB) {
+      return -1;
+    }
+    if (keyA > keyB) {
+      return 1;
+    }
+    return 0;
+  })
+  .map((item) => {
+    const {key, presence} = item;
+    switch (presence) {
+      case 'onlyFirst':
+        return `  - ${key}: ${item.value}`;
+      case 'onlySecond':
+        return `  + ${key}: ${item.value}`;
+      case 'bothDifferent':
+        return `  - ${key}: ${item.firstValue}\n  + ${key}: ${item.secondValue}`;
+      case 'bothSame':
+        return `    ${key}: ${item.value}`;
+    }
+  })
+  console.log(`{\n${result.join('\n')}\n}`)
+  return `{\n${result.join('\n')}\n}`
+}
+
+export default makeConsoleDiff;
